fc3e84218bca1b4ea78ad6fa3389169dc5c6f2fa
sheisuka <xxtornex@gmail.com>
2024-02-08 16:26:12 +0700

    Move setting data to .env

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -1,18 +1,21 @@
+from dotenv import load_dotenv
+from os import environ
 from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / "../.env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "FAKE"
+SECRET_KEY = environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environ["DJANGO_DEBUG"]
 
 ALLOWED_HOSTS = []
 
