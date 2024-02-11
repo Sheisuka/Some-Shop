f5b74673c219c3d8c5914a0d60e04f33b03d4317
sheisuka <xxtornex@gmail.com>
2024-02-11 15:10:16 +0700

    Add higher priority for environment over .env

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / "../.env")
+load_dotenv(BASE_DIR / "../.env", override=False) # Env vars have higher priority 
 
 SECRET_KEY = environ.get("DJANGO_SECRET_KEY", "django-secret-key")
 
