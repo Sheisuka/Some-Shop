5cb0ff50db2adf1834cc15c784fa1a2269c81346
sheisuka <xxtornex@gmail.com>
2024-02-11 15:20:31 +0700

    Add item list&detail view

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,1 +1,8 @@
-# from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def item_list(request):
+    return HttpResponse("<body>Список элементов</body>")
+
+def item_detail(request, pk):
+    return HttpResponse("<body>Подробно элемент</body>")
