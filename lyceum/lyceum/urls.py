3f4a426553972a7605faed63efbe41920b1294c2
sheisuka <xxtornex@gmail.com>
2024-02-11 15:15:55 +0700

    Add urlpatterns

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
+from homepage.views import home
+from catalog.views import item_list, item_detail
+from about.views import description
+
+
 urlpatterns = [
+    path("", home),
+    path("about", description),
     path("admin/", admin.site.urls),
+    path("catalog/", item_list),
+    path("catalog/<int:pk>/", item_detail),
 ]
