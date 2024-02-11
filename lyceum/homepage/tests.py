a42d6525af43b5f53d23035efc0b731c1afe542c
sheisuka <xxtornex@gmail.com>
2024-02-11 15:28:55 +0700

    Add static url test

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -1,1 +1,7 @@
-# from django.test import TestCase
+from django.test import TestCase, Client
+
+
+class StaticURLTests(TestCase):
+    def test_homepage_endpoint(self):
+        response = Client.get("/")
+        self.assertEqual(response.status_code, 200)
