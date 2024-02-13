15a18263a4c0778a6c8c6dd15f3d1b4c289041eb
sheisuka <xxtornex@gmail.com>
2024-02-13 23:29:51 +0700

    Add endpoint tests

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -5,3 +5,12 @@ class StaticURLTests(TestCase):
     def test_homepage_endpoint(self):
         response = Client().get("/")
         self.assertEqual(response.status_code, 200)
+    
+    def test_coffee_code_endpoint(self):
+        response = Client().get("/coffee/")
+        self.assertEqual(response.status_code, 418)
+
+    def test_coffee_content_endpoint(self):
+        response = Client().get("/coffee/")
+        self.assertInHTML("Я чайник", response.content.decode())
+
