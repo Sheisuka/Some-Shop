763460bd882f2e47f030ab56c55e1c256325eea4
sheisuka <xxtornex@gmail.com>
2024-02-13 23:29:37 +0700

    Add 418 view

--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<body>Главная</body>")
+
+def coffee(request):
+    response = HttpResponse("<body>Я чайник</body>")
+    response.status_code = 418
+    return response
