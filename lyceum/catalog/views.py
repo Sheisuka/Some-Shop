c88560480e6f5f87f6bdc6e4e8c8b5e5a72dd861
sheisuka <xxtornex@gmail.com>
2024-02-13 23:30:09 +0700

    Add re_path view

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 def item_list(request):
     return HttpResponse("<body>Список элементов</body>")
 
-
 def item_detail(request, pk):
     return HttpResponse("<body>Подробно элемент</body>")
+
+def re_view(request):
+    return HttpResponse(f"<body>{request.path.split('/')[-2]}</body>")
