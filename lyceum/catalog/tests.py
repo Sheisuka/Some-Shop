2b890c515229e7db2538c24e2382f71b369feefb
sheisuka <xxtornex@gmail.com>
2024-02-13 23:30:24 +0700

    Add tests for re_path

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -13,3 +13,19 @@ class StaticURLTests(TestCase):
     def test_item_detail_negative_endpoint(self):
         response = Client().get("/catalog/-10")
         self.assertEqual(response.status_code, 404)
+    
+    def test_re_negative(self):
+        response = Client().get("/catalog/re/-10/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_re_letters(self):
+        response = Client().get("/catalog/re/abc/")
+        self.assertEqual(response.status_code, 404)   
+    
+    def test_re_zero(self):
+        response = Client().get("/catalog/re/0/")
+        self.assertEqual(response.status_code, 404)
+    
+    def test_re_endpoint(self):
+        response = Client().get("/catalog/re/100/")
+        self.assertEqual(response.status_code, 200)
