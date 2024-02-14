bb2cb7c976e2cf7f8e59eba86c5f94283c21677c
sheisuka <xxtornex@gmail.com>
2024-02-14 19:53:11 +0700

    Add catalog urls

---
+++ lyceum/catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, re_path, register_converter
+
+from . import converters, views
+
+register_converter(converters.PositiveIntegerConverter, "pint")
+
+urlpatterns = [
+    path("", views.item_list),
+    path("<int:pk>/", views.item_detail),
+    re_path(r"^re\/[1-9][0-9]*\/$", views.re_view),
+    path("converter/<pint:value>/", views.converter_view),
+]
