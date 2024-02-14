6b17b6105ff379c9b5cca64e306fd2c0cf753fc7
sheisuka <xxtornex@gmail.com>
2024-02-14 19:52:59 +0700

    Separate urls by apps

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,18 +1,14 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 
-from about.views import description
-from catalog.views import item_detail, item_list, re_view
 from homepage.views import coffee, home
 
 
 urlpatterns = [
     path("", home),
-    path("about/", description),
+    path("about/", include("about.urls")),
     path("admin/", admin.site.urls),
-    path("catalog/", item_list),
-    path("catalog/<int:pk>/", item_detail),
-    re_path(r"^catalog\/re\/[1-9][0-9]*\/$", re_view),
+    path("catalog/", include("catalog.urls")),
     path("coffee/", coffee)
 ]
 
