1e2c9566a229ef2462f03fe63de9f5a7b28f0f1a
sheisuka <xxtornex@gmail.com>
2024-02-17 01:49:16 +0700

    Add reversing middleware

--- lyceum/lyceum/middleware.py
+++ lyceum/lyceum/middleware.py
@@ -2,16 +2,15 @@ class ReverseMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.count = 0
-    
-    def __call__(self, request):
-        self.count += 1
 
+    def __call__(self, request):
         response = self.get_response(request)
 
-        content = response.content.decode()
+        self.count += 1
         if self.count % 10 == 0:
+            content = response.content.decode()
             self.count = 0
             content = " ".join([word[::-1] for word in content.split()])
-        response.content = content.encode()
+            response.content = content.encode()
 
         return response
