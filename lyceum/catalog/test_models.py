10dce76ddae7cbf054b0a5a59e6920f270cd497d
sheisuka <xxtornex@gmail.com>
2024-02-23 21:52:34 +0700

    Add model tests

---
+++ lyceum/catalog/test_models.py
@@ -0,0 +1,91 @@
+import django
+
+import catalog
+
+
+class ModelTests(django.test.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.category = catalog.models.Category.objects.create(
+            is_published=True,
+            name="Тестовая категория",
+            slug="test-category-slug",
+            weight=100,
+        )
+        cls.tag = catalog.models.Tag.objects.create(
+            is_published=True,
+            name="Тестовый тег",
+            slug="test-tag-slug",
+        )
+
+    def test_unable_create_category_zero_weight(self):
+        category_count = catalog.models.Category.objects.count()
+        with self.assertRaises(django.core.exceptions.ValidationError):
+            self.category = catalog.models.Category(
+                name="Тестовая категория", slug="test-category-slug", weight=0
+            )
+            self.category.full_clean()
+            self.category.save()
+
+        self.assertEqual(
+            catalog.models.Category.objects.count(), category_count
+        )
+
+    def test_unable_create_category_heavy_weight(self):
+        category_count = catalog.models.Category.objects.count()
+        with self.assertRaises(django.core.exceptions.ValidationError):
+            self.category = catalog.models.Category(
+                name="Тестовая категория",
+                slug="test-category-slug",
+                weight=32768,
+            )
+            self.category.full_clean()
+            self.category.save()
+
+        self.assertEqual(
+            catalog.models.Category.objects.count(), category_count
+        )
+
+    def test_create_item(self):
+        item_count = catalog.models.Item.objects.count()
+
+        self.item = catalog.models.Item.objects.create(
+            name="Тестовый товар",
+            category=self.category,
+            text="Превосходно",
+        )
+
+        self.item.tags.add(self.tag)
+        self.item.full_clean()
+        self.item.save()
+
+        self.assertEqual(catalog.models.Item.objects.count(), item_count + 1)
+
+    def test_item_not_gorgeous_validator(self):
+        item_count = catalog.models.Item.objects.count()
+        with self.assertRaises(django.core.exceptions.ValidationError):
+            self.item = catalog.models.Item.objects.create(
+                name="Товар тестовый",
+                category=self.category,
+                text="Данный товар это очень плохо",
+            )
+            self.item.tags.add(self.tag)
+            self.item.full_clean()
+            self.item.save()
+
+        self.assertEqual(catalog.models.Item.objects.count(), item_count)
+
+    def test_item_gorgeous_validator(self):
+        item_count = catalog.models.Item.objects.count()
+        self.item = catalog.models.Item.objects.create(
+            name="Товар тестовый",
+            category=self.category,
+            text="Данный товар это очень роскошно",
+        )
+        self.item.tags.add(self.tag)
+        self.item.full_clean()
+        self.item.save()
+
+        self.assertEqual(catalog.models.Item.objects.count(), item_count + 1)
---
+++ lyceum/catalog/test_views.py
@@ -0,0 +1,77 @@
+from django.test import TestCase
+
+
+class ItemListTests(TestCase):
+    def test_item_list_code(self):
+        response = self.client.get("/catalog/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_item_list_content(self):
+        response = self.client.get("/catalog/")
+        self.assertEqual(response.content, "Список элементов".encode())
+
+
+class ItemDetailTests(TestCase):
+    def test_item_detail_zero_code(self):
+        response = self.client.get("/catalog/0/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_item_detail_negative_code(self):
+        response = self.client.get("/catalog/-10")
+        self.assertEqual(response.status_code, 404)
+
+    def test_item_detail_letters_code(self):
+        response = self.client.get("/catalog/abc")
+        self.assertEqual(response.status_code, 404)
+
+    def test_item_detail_symbols_code(self):
+        response = self.client.get("/catalog/@-!--/")
+        self.assertEqual(response.status_code, 404)
+
+
+class ReTests(TestCase):
+    def test_re_negative_code(self):
+        response = self.client.get("/catalog/re/-10/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_re_letters_code(self):
+        response = self.client.get("/catalog/re/abc/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_re_zero_code(self):
+        response = self.client.get("/catalog/re/0/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_re_code(self):
+        response = self.client.get("/catalog/re/100/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_re_content(self):
+        response = self.client.get("/catalog/re/100/")
+        self.assertEqual(response.content, "100".encode())
+
+    def test_re_symbols_code(self):
+        response = self.client.get("/catalog/re/@-!--/")
+        self.assertEqual(response.status_code, 404)
+
+
+class ConverterTests(TestCase):
+    def test_converter_code(self):
+        response = self.client.get("/catalog/converter/100/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_converter_content(self):
+        response = self.client.get("/catalog/converter/50/")
+        self.assertEqual(response.content, "50".encode())
+
+    def test_converter_letters_code(self):
+        response = self.client.get("/catalog/converter/abc/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_converter_negative_code(self):
+        response = self.client.get("/catalog/converter/-10/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_converter_symbols_code(self):
+        response = self.client.get("/catalog/converter/@-!--/")
+        self.assertEqual(response.status_code, 404)
