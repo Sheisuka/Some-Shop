6c963eb23b9a542d610d29f5ca25a71e1612602b
sheisuka <xxtornex@gmail.com>
2024-02-25 20:59:22 +0700

    Fix with black

--- lyceum/catalog/validators.py
+++ lyceum/catalog/validators.py
@@ -13,20 +13,3 @@ def gorgeous_validator(value):
         raise django.core.exceptions.ValidationError(
             ('В тексте должно содержаться "роскошно" или "превосходно"'),
         )
-
-
-class ValidateMustContain:
-    def __init__(self, *words, message=None):
-        self.words_to_contain = words
-        self.message = (
-            message
-            or f"Должно содержаться одно из слов: \n\t{' '.join(words)}"
-        )
-
-    def __call__(self, value):
-        value_splitted = set(value.split())
-        for word in self.words_to_contain:
-            if word in value_splitted:
-                return
-
-        raise django.core.exceptions.ValidationError(self.message)
