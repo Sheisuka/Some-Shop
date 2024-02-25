0ebb182fbce18d4248d2f1e8d4cff420744f2bb8
sheisuka <xxtornex@gmail.com>
2024-02-25 13:59:04 +0700

    Fix validator

--- lyceum/catalog/validators.py
+++ lyceum/catalog/validators.py
@@ -2,8 +2,7 @@ import django.core.exceptions
 
 
 def item_text_validator(value):
-    words = value.split()
-    if not ("превосходно" in words or "роскошно" in words):
+    if not ("превосходно" in value or "роскошно" in value):
         raise django.core.exceptions.ValidationError(
             ('В тексте должно содержаться "роскошно" или "превосходно"'),
         )
