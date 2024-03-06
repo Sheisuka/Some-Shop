566864df978549162f2a6cf7865fc77277004ff0
sheisuka <xxtornex@gmail.com>
2024-03-07 02:28:10 +0700

    Change admin panel view

--- lyceum/catalog/admin.py
+++ lyceum/catalog/admin.py
@@ -2,13 +2,23 @@ import django.contrib
 
 import catalog.models
 
-__all__ = ["ImageAdmin", "ImageInline", "ItemAdmin"]
+__all__ = ["MainImageInline", "ImageInline", "ItemAdmin"]
+
+
+django.contrib.admin.site.register(catalog.models.Category)
+django.contrib.admin.site.register(catalog.models.Tag)
+django.contrib.admin.site.register(catalog.models.MainImage)
+django.contrib.admin.site.register(catalog.models.Image)
+
+
+class MainImageInline(django.contrib.admin.TabularInline):
+    model = catalog.models.MainImage
+    fields = ("image",)
 
 
 class ImageInline(django.contrib.admin.TabularInline):
     model = catalog.models.Image
-    readonly_fields = ("image_tmb",)
-    extra = 0
+    fields = ("image",)
 
 
 @django.contrib.admin.register(catalog.models.Item)
@@ -16,37 +26,10 @@ class ItemAdmin(django.contrib.admin.ModelAdmin):
     list_display = (
         catalog.models.Item.name.field.name,
         catalog.models.Item.is_published.field.name,
-        "get_main_image",
+        catalog.models.Item.image_tmb,
     )
     list_editable = (catalog.models.Item.is_published.field.name,)
     list_display_links = (catalog.models.Item.name.field.name,)
-    fields = (
-        "name",
-        "is_published",
-        "text",
-        "category",
-        "tags",
-        "MainImage",
-        "get_main_image",
-    )
-    readonly_fields = ("get_main_image",)
+    readonly_fields = (catalog.models.Item.image_tmb,)
     filter_horizontal = ("tags",)
-    inlines = [ImageInline]
-
-    def get_main_image(self, obj):
-        return obj.MainImage.image_tmb()
-
-    get_main_image.short_description = "превью"
-
-
-@django.contrib.admin.register(catalog.models.Image)
-class ImageAdmin(django.contrib.admin.ModelAdmin):
-    list_display = (
-        "image_tmb",
-        "image",
-    )
-    readonly_fields = ("image_tmb",)
-
-
-django.contrib.admin.site.register(catalog.models.Category)
-django.contrib.admin.site.register(catalog.models.Tag)
+    inlines = [MainImageInline, ImageInline]
