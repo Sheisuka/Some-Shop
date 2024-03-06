376e5c9ee116c11bd4b64c4fc8f94faeda48c36a
sheisuka <xxtornex@gmail.com>
2024-03-07 02:26:44 +0700

    Optimize queries

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,14 +1,43 @@
+import django.db.models
 import django.http
 import django.shortcuts
+
+import catalog.models
 
 __all__ = ["item_detail", "item_list"]
 
 
 def item_list(request):
     template = "catalog/item_list.html"
-    return django.shortcuts.render(request, template)
+
+    categories = catalog.models.Category.objects.filter(
+        is_published=True,
+    ).only("name").order_by("name")
+
+    item_query = catalog.models.Item.objects.filter(is_published=True).only(
+        "name", "text", "tags", "main_image", "category",
+    )
+    tag_query = catalog.models.Tag.objects.filter(is_published=True).only(
+        "name",
+    )
+
+    categories = categories.prefetch_related(
+        django.db.models.Prefetch("items", queryset=item_query),
+        django.db.models.Prefetch("items__tags", queryset=tag_query),
+        "items__main_image",
+    )
+    return django.shortcuts.render(
+        request,
+        template,
+        {"categories": categories},
+    )
 
 
 def item_detail(request, pk):
     template = "catalog/item.html"
-    return django.shortcuts.render(request, template)
+    item = django.shortcuts.get_object_or_404(
+        catalog.models.Item.objects,
+        pk=pk,
+        is_published=True,
+    )
+    return django.shortcuts.render(request, template, {"item": item})
--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -1,15 +1,38 @@
 import http
 
+import django.db.models
 import django.http
 import django.shortcuts
+
+import catalog.models
 
 __all__ = ["coffee", "home"]
 
 
 def home(request):
     template = "homepage/main.html"
-
-    return django.shortcuts.render(request, template)
+    main_items = (
+        catalog.models.Item.objects.select_related("category", "main_image")
+        .prefetch_related(
+            django.db.models.Prefetch(
+                "tags",
+                queryset=catalog.models.Tag.objects.filter(
+                    is_published=True,
+                ).only("name"),
+            ),
+        )
+        .filter(is_published=True, is_on_main=True)
+        .only(
+            "pk",
+            "name",
+            "text",
+            "category__name",
+            "tags",
+            "main_image__image",
+        )
+        .order_by("name")
+    )
+    return django.shortcuts.render(request, template, {"items": main_items})
 
 
 def coffee(request):
