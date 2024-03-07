fb93690afd40c3582e69572b339708642bd6ed67
sheisuka <xxtornex@gmail.com>
2024-03-07 16:50:55 +0700

    Optimize query

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -36,9 +36,32 @@ def item_list(request):
 
 def item_detail(request, pk):
     template = "catalog/item.html"
-    item = django.shortcuts.get_object_or_404(
-        catalog.models.Item.objects,
-        pk=pk,
-        is_published=True,
+
+    tags_query = catalog.models.Tag.objects.filter(is_published=True).only(
+        "name",
     )
+    images_query = catalog.models.Image.objects.only("id", "image", "item_id")
+    items_query = (
+        catalog.models.Item.objects.filter(is_published=True)
+        .select_related("category", "main_image")
+        .prefetch_related(
+            django.db.models.Prefetch(
+                "tags",
+                queryset=tags_query,
+            ),
+        )
+        .prefetch_related(
+            django.db.models.Prefetch(
+                "images",
+                queryset=images_query,
+            ),
+        )
+        .only("name", "text", "category__name", "main_image__image")
+    )
+
+    item = django.shortcuts.get_object_or_404(
+        items_query,
+        pk=pk,
+    )
+
     return django.shortcuts.render(request, template, {"item": item})
